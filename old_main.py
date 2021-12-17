1ab0bb1bab6363232e22d0f13e2a2b29dcd1a6c1
tumu <m1716065tm@ug.jwu.ac.jp>
2021-12-17 19:25:44 +0900

    new_transformers_version

--- old_main.py
+++ old_main.py
@@ -1,5 +1,4 @@
-
-#インポート
+#必要なライブラリ等のimport
 import pandas as pd
 import csv
 from sklearn.model_selection import train_test_split
@@ -26,25 +25,57 @@ from transformers import (
 import textwrap
 from tqdm.auto import tqdm
 import shutil
+import argparse  
 
+#オプションを設定する
+parser = argparse.ArgumentParser(description='Using mT5 with ABCI')
+parser.add_argument('input_file', help='corpus')
+parser.add_argument('-e','--epochs', help='epochs',type=int,default=4)
+parser.add_argument('--zip', action='store_true',help='Save the model as a zip file')
+parser.add_argument('--result',action='store_true',help='Output the result as a tsv file')    
 
-#関数定義
-
+args = parser.parse_args() 
+#パラメータ等の設定
 # 事前学習済みモデル
-PRETRAINED_MODEL_NAME = "google/mt5-small"
+PRETRAINED_MODEL_NAME = "google/mt5-base"
 
 # 転移学習済みモデルを保存する場所
-DATA_DIR = "/content/data"
 MODEL_DIR = "/content/model"
 
-INPUT_tsv = '/content/corpus_Python-JPN/Corpus-DS/DS_BM.tsv'
-# INPUT_tsv = '/content/corpus_Python-JPN/Corpus-DS/parameter/DS_B_diff_shuffle.tsv'
-
+# コーパスがある場所
+INPUT_tsv = args.input_file
+# INPUT_tsv = '/content/corpus_Python-JPN/Corpus-DS/DS_B.tsv'
 
 # GPU利用有無
 USE_GPU = torch.cuda.is_available()
 
+# 各種ハイパーパラメータ
+args_dict = dict(
+    data_dir="/content/data",  # データセットのディレクトリ
+    model_name_or_path=PRETRAINED_MODEL_NAME,
+    tokenizer_name_or_path=PRETRAINED_MODEL_NAME,
 
+    learning_rate=3e-4,
+    weight_decay=0.0,
+    adam_epsilon=1e-8,
+    warmup_steps=0,
+    gradient_accumulation_steps=1,
+
+    # max_input_length=512,
+    # max_target_length=4,
+    # train_batch_size=8,
+    # eval_batch_size=8,
+    # num_train_epochs=4,
+
+    n_gpu=1 if USE_GPU else 0,
+    early_stop_callback=False,
+    fp_16=False,
+    opt_level='O1',
+    max_grad_norm=1.0,
+    seed=42,
+)
+
+#関数の設定
 def from_tsv(f_path):
   df = pd.read_table(f_path, header=None)
 
@@ -66,7 +97,6 @@ def from_tsv(f_path):
 
   return len(df_train), len(df_valid), len(df_test)
 
-
 # 乱数シードの設定
 def set_seed(seed):
     random.seed(seed)
@@ -74,7 +104,6 @@ def set_seed(seed):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-
 
 class TsvDataset(Dataset):
     def __init__(self, tokenizer, data_dir, type_path, input_max_len=512, target_max_len=512):
@@ -131,40 +160,16 @@ class TsvDataset(Dataset):
                 self.inputs.append(tokenized_inputs)
                 self.targets.append(tokenized_targets)
 
-
 class MT5FineTuner(pl.LightningModule):
-    def __init__(self, 
-                 data_dir=DATA_DIR,
-                 model_name_or_path=PRETRAINED_MODEL_NAME,
-                 tokenizer_name_or_path=PRETRAINED_MODEL_NAME,
-
-                 learning_rate=3e-4,
-                 weight_decay=0.0,
-                 adam_epsilon=1e-8,
-                 warmup_steps=0,
-                 gradient_accumulation_steps=1,
-
-                 max_input_length=128,
-                 max_target_length=128,
-                 train_batch_size=4,
-                 eval_batch_size=4,
-                 num_train_epochs=4,
-
-                 n_gpu=1 if USE_GPU else 0,
-                 early_stop_callback=False,
-                 fp_16=False,
-                 opt_level='O2',
-                 max_grad_norm=1.0,
-                 seed=42,
-                 ):
+    def __init__(self, hparams):
         super().__init__()
-        self.save_hyperparameters()
+        self.hparams = hparams
 
         # 事前学習済みモデルの読み込み
-        self.model = MT5ForConditionalGeneration.from_pretrained(self.hparams.model_name_or_path)
+        self.model = MT5ForConditionalGeneration.from_pretrained(hparams.model_name_or_path)
 
         # トークナイザーの読み込み
-        self.tokenizer = MT5Tokenizer.from_pretrained(self.hparams.tokenizer_name_or_path, is_fast=True)
+        self.tokenizer = MT5Tokenizer.from_pretrained(hparams.tokenizer_name_or_path, is_fast=True)
         self.tokenizer.add_tokens(additional_special_tokens)
 
     def forward(self, input_ids, attention_mask=None, decoder_input_ids=None, 
@@ -287,51 +292,8 @@ class MT5FineTuner(pl.LightningModule):
         """バリデーションデータローダーを作成する"""
         return DataLoader(self.val_dataset, 
                           batch_size=self.hparams.eval_batch_size, 
-                          num_workers=4) 
+                          num_workers=4)
 
-# 量子化
-def quantize_transform(model):
-    model = torch.quantization.quantize_dynamic(
-        model, {torch.nn.Linear}, dtype=torch.qint8
-    )
-    return model
-
-def translate(text):
-    input_ids = tokenizer(text, return_tensors='pt').input_ids
-    if USE_GPU:
-        input_ids = input_ids.cuda()
-    predict = trained_model.generate(input_ids)
-    print(tokenizer.decode(predict[0], skip_special_tokens=True))
-
-def translate_beam(text: str, beams=5, max_length=64):
-        trained_model.config.update({"num_beams": beams})
-        input_ids = tokenizer.encode_plus(
-            text,
-            add_special_tokens=True,
-            max_length=max_length,
-            # padding="max_length",
-            padding="do_not_pad",
-            truncation=True,
-            return_tensors='pt').input_ids
-        if USE_GPU:
-            input_ids = input_ids.cuda()
-        predict = trained_model.generate(input_ids,
-                                          return_dict_in_generate=True,
-                                          output_scores=True,
-                                          length_penalty=8.0,
-                                          max_length=max_length,
-                                          num_return_sequences=beams,
-                                          early_stopping=True)
-        pred_list = sorted([[tokenizer.decode(predict.sequences[i], skip_special_tokens=True),
-                             predict.sequences_scores[i].item()] for i in range(len(predict))], key=lambda x: x[1], reverse=True)
-        sentences_list = [i[0] for i in pred_list]
-        scores_list = [i[1] for i in pred_list]
-        # return sentences_list, scores_list
-        return sentences_list[0]
-
-
-
-#プログラムの実行
 
 train_num, valid_num, test_num = from_tsv(INPUT_tsv)
 
@@ -339,65 +301,52 @@ INPUT_tsv = INPUT_tsv[INPUT_tsv.rfind('/')+1:]
 
 set_seed(42)
 
+#色々お試し中
+# var_tokens = []
+# name_val_tokens = []
 
+# # for idx in range(1, 10):
+# #   var_tokens.append(f'<var{idx}>')
 
-
-var_tokens = []
-name_val_tokens = []
-
-# for idx in range(1, 10):
+# for idx in range(1, 7):
 #   var_tokens.append(f'<var{idx}>')
-
-for idx in range(1, 7):
-  var_tokens.append(f'<var{idx}>')
-  name_val_tokens.append(f'<name{idx}>')
-  name_val_tokens.append(f'<val{idx}>')
+#   name_val_tokens.append(f'<name{idx}>')
+#   name_val_tokens.append(f'<val{idx}>')
 
 
-# パラメータ化用の特殊トークン
-# additional_special_tokens = ['<A>', '<B>', '<C>', '<D>', '<E>', '<a>', '<b>', '<c>', '<d>', '<e>']
-additional_special_tokens = name_val_tokens
+  # パラメータ化用の特殊トークン
+additional_special_tokens = ['<A>', '<B>', '<C>', '<D>', '<E>', '<a>', '<b>', '<c>', '<d>', '<e>']
 
-# トークナイザー（SentencePiece）モデルの読み込み
-tokenizer = MT5Tokenizer.from_pretrained(PRETRAINED_MODEL_NAME, is_fast=True)
-tokenizer.add_tokens(additional_special_tokens)
+# 学習に用いるハイパーパラメータを設定する
+args_dict.update({
+    "max_input_length":  128,  # 入力文の最大トークン数
+    "max_target_length": 128,  # 出力文の最大トークン数
+    "train_batch_size":  4,
+    "eval_batch_size":   4,
+    "num_train_epochs":  args.epochs,
+    # "num_train_epochs":  4,
+    })
+print(args_dict)
+args = argparse.Namespace(**args_dict)
 
-# テストデータセットの読み込み
-test_dataset = TsvDataset(tokenizer, DATA_DIR, "test.tsv", 
-                           input_max_len=128, target_max_len=128)
-
-# # cnt = 0
-# for data in test_dataset:
-#     print("A. 入力データの元になる文字列")
-#     print(tokenizer.decode(data["source_ids"]))
-#     print()
-#     print("B. 入力データ（Aの文字列がトークナイズされたトークンID列）")
-#     print(data["source_ids"])
-#     print()
-#     print("C. 出力データの元になる文字列")
-#     print(tokenizer.decode(data["target_ids"]))
-#     print()
-#     print("D. 出力データ（Cの文字列がトークナイズされたトークンID列）")
-#     print(data["target_ids"])
-#     break
-#     # cnt += 1
-#     # if cnt == 5:
-#     #     break
-
+# checkpoint_callback = pl.callbacks.ModelCheckpoint(
+#     "/content/checkpoints", 
+#     monitor="val_loss", mode="min", save_top_k=1
+# )
 
 train_params = dict(
-    accumulate_grad_batches = 1,
-    gpus = 1 if USE_GPU else 0,
-    max_epochs = 1,
-    precision= 32,
-    gradient_clip_val=1.0,
-    # amp_level=args.opt_level,
+    accumulate_grad_batches=args.gradient_accumulation_steps,
+    gpus=args.n_gpu,
+    max_epochs=args.num_train_epochs,
+    precision= 16 if args.fp_16 else 32,
+    amp_level=args.opt_level,
+    gradient_clip_val=args.max_grad_norm,
     # checkpoint_callback=checkpoint_callback,
 )
 
 
 # 転移学習の実行（GPUを利用すれば1エポック10分程度）
-model = MT5FineTuner()
+model = MT5FineTuner(args)
 trainer = pl.Trainer(**train_params)
 trainer.fit(model)
 
@@ -407,14 +356,11 @@ model.model.save_pretrained(MODEL_DIR)
 
 del model
 
-
-
 # トークナイザー（SentencePiece）
 tokenizer = MT5Tokenizer.from_pretrained(MODEL_DIR, is_fast=True)
 
 # 学習済みモデル
 trained_model = MT5ForConditionalGeneration.from_pretrained(MODEL_DIR)
-q_trained_model = quantize_transform(MT5ForConditionalGeneration.from_pretrained(MODEL_DIR))
 
 # GPUの利用有無
 USE_GPU = torch.cuda.is_available()
@@ -422,24 +368,10 @@ if USE_GPU:
     trained_model.cuda()
 
 
-# 学習済みモデルのパラメータ数
-params = 0
-for p in trained_model.parameters():
-    if p.requires_grad:
-        params += p.numel()
-
-q_params = 0
-for p in q_trained_model.parameters():
-    if p.requires_grad:
-        q_params += p.numel()
-
-print('Number of parameters : ', params, '-->', q_params)
-
-
 # テストデータの読み込み
-test_dataset = TsvDataset(tokenizer, DATA_DIR, "test.tsv", 
-                          input_max_len=128, 
-                          target_max_len=128)
+test_dataset = TsvDataset(tokenizer, args_dict["data_dir"], "test.tsv", 
+                          input_max_len=args.max_input_length, 
+                          target_max_len=args.max_target_length)
 
 test_loader = DataLoader(test_dataset, batch_size=32, num_workers=4)
 
@@ -450,23 +382,17 @@ targets = []
 sources = []
 
 for batch in tqdm(test_loader):
-    trained_model.config.update({"num_beams": 5})
     input_ids = batch['source_ids']
     input_mask = batch['source_mask']
-
     if USE_GPU:
         input_ids = input_ids.cuda()
         input_mask = input_mask.cuda()
 
-    outs = trained_model.generate(
-        input_ids=input_ids, 
+    outs = trained_model.generate(input_ids=input_ids, 
         attention_mask=input_mask, 
-        max_length=128,
-        num_return_sequences=5,
-        # length_penalty=8.0,
+        max_length=args.max_target_length,
         return_dict_in_generate=True,
-        # output_scores=True,
-        early_stopping=True)
+        output_scores=True)
 
     dec = [tokenizer.decode(ids, skip_special_tokens=True, 
                             clean_up_tokenization_spaces=False) 
@@ -478,24 +404,20 @@ for batch in tqdm(test_loader):
                                clean_up_tokenization_spaces=False) 
                 for ids in batch["source_ids"]]
 
-    outputs.extend(dec[0])
+    outputs.extend(dec)
     targets.extend(target)
     sources.extend(source)
 
 
 df = pd.DataFrame(list(zip(sources, targets, outputs)), columns = ['JPN', 'Py', ' Predict'])
-df.head()
-
 
 INPUT_tsv = INPUT_tsv.replace('.tsv', '')
 OUTPUT_tsv = f'result_{INPUT_tsv}_forMT5.tsv'
-df.to_csv(OUTPUT_tsv, index=False, header=False, sep='\t', quoting=csv.QUOTE_NONE, doublequote=False, escapechar='\n')
 
-
-
-shutil.make_archive('modelzip','zip',root_dir='model')
-
-
-
-input = 'データフレーム <name2> 内の <val4> とコラム <val1> に重複はあるのかどうかチェックする'
-translate_beam(input)
+if args.zip == True:
+  df.to_csv(OUTPUT_tsv, index=False, header=False, sep='\t', quoting=csv.QUOTE_NONE, doublequote=False, escapechar='\n')
+elif args.result==True:
+  shutil.make_archive('/content/modelzip','zip',root_dir='/content/model')
+else:
+  df.to_csv(OUTPUT_tsv, index=False, header=False, sep='\t', quoting=csv.QUOTE_NONE, doublequote=False, escapechar='\n')
+  shutil.make_archive('/content/modelzip','zip',root_dir='/content/model')
